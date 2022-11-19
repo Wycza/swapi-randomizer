b5bc466954251f856f91dcb0ccda9e508c646155
Mateusz Wyczawski <wyczawski93@gmail.com>
2022-11-19 23:04:31 +0100

    Write GameSelector component tests

--- src/app/components/game-selector/game-selector.component.spec.ts
+++ src/app/components/game-selector/game-selector.component.spec.ts
@@ -1,23 +1,57 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { NgxsModule, Store } from '@ngxs/store';
+import { SwapiType } from 'src/app/enums/swapiType.enum';
+import { GameState } from 'src/app/store/game/game.state';
 
 import { GameSelectorComponent } from './game-selector.component';
 
 describe('GameSelectorComponent', () => {
   let component: GameSelectorComponent;
   let fixture: ComponentFixture<GameSelectorComponent>;
+  let store: Store;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ GameSelectorComponent ]
+      declarations: [GameSelectorComponent],
+      imports: [
+        NgxsModule.forRoot([GameState], {
+          developmentMode: true,
+        }),
+        HttpClientTestingModule,
+        FormsModule,
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(GameSelectorComponent);
+    store = TestBed.inject(Store);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
+  });
+
+  it('should change game type', () => {
+    // Arrange
+    const newMode = SwapiType.Starships;
+
+    store.reset({
+      ...store.snapshot(),
+      gameState: {
+        currentGameMode: SwapiType.People,
+      }
+    });
+
+    // Act
+    fixture.detectChanges();
+    component.onGameChange(newMode);
+    const mode = store.selectSnapshot(GameState.getCurrentGameMode);
+
+    // Assert    
+    expect(mode).toBe(newMode);
   });
 });
