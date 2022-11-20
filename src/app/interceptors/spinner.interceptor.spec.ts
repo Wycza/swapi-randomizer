8f0660808d6bd25609e6aeee66afcd504fe51cc3
Mateusz Wyczawski <wyczawski93@gmail.com>
2022-11-20 14:41:06 +0100

    Write Spinner interceptor tests

--- src/app/interceptors/spinner.interceptor.spec.ts
+++ src/app/interceptors/spinner.interceptor.spec.ts
@@ -1,16 +1,62 @@
-import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
+import { SpinnerService } from '../services/spinner.service';
 
 import { SpinnerInterceptor } from './spinner.interceptor';
+import { SwapiService } from '../api/swapi.service';
+import { finalize } from 'rxjs';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
-describe('LoadingInterceptor', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    providers: [
-      SpinnerInterceptor
+describe('SpinnerInterceptor', () => {
+  let interceptor: SpinnerInterceptor;
+  let httpMock: HttpTestingController;
+  let swapiService: SwapiService;
+  const swapiBaseUrl = 'https://www.swapi.tech/api';
+  const spinnerServiceSpy = jasmine.createSpyObj('SpinnerService', ['setLoading']);
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        SpinnerInterceptor,
+        SwapiService,
+        { provide: SpinnerService, useValue: spinnerServiceSpy },
+        { provide: HTTP_INTERCEPTORS, useClass: SpinnerInterceptor, multi: true }
+      ],
+      imports: [
+        HttpClientTestingModule,
       ]
-  }));
+    });
+
+    interceptor = TestBed.inject(SpinnerInterceptor);
+    httpMock = TestBed.inject(HttpTestingController);
+    swapiService = TestBed.inject(SwapiService);
+  });
 
   it('should be created', () => {
-    const interceptor: SpinnerInterceptor = TestBed.inject(SpinnerInterceptor);
     expect(interceptor).toBeTruthy();
   });
+
+  it('should show and then hide spinner', waitForAsync(() => {
+    // Arrange
+    const id = 1;
+
+    // Act & Assert
+    swapiService.getPeopleById(id)
+      .pipe(
+        // In finalize we should hide interceptor
+        finalize(() => expect(spinnerServiceSpy.setLoading).toHaveBeenCalledWith(false)))
+      .subscribe(() => {
+        expect(spinnerServiceSpy.setLoading).toHaveBeenCalledWith(true);
+      });
+
+    httpMock.expectOne(`${swapiBaseUrl}/people/${id}`).flush({
+      result: {
+        description: '',
+        properties: {
+          mass: '',
+        },
+        uid: ''
+      }
+    });
+  }));
 });
